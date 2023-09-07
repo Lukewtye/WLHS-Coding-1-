83d122b4e8179ac20f09c02c8414b4453d98a42b
Lukewtye <143744427+Lukewtye@users.noreply.github.com>
2023-09-07 10:34:01 -0700

    Add files via upload

---
+++ my_python_code/Unit 1 Practice variables&printing.py
@@ -0,0 +1,13 @@
+KM_IN_MILES = 1.61
+SEC_IN_MIN=60
+
+
+seconds_in_race_time = 42 * SEC_IN_MIN + 42
+
+print(seconds_in_race_time)
+
+print("The time in seconds is " + str(seconds_in_race_time) )
+
+print("The time in seconds is",seconds_in_race_time)
+
+print(type(seconds_in_race_time))
---
+++ my_python_code/geometry_fun.py
@@ -0,0 +1,14 @@
+'''
+Name: Luke Tye
+Date: 9/7/23
+Description:
+This is my Unit 1 Coding Final Project. These are the objectives:
+
+Write a program named geometry_fun.py which prompts a user for a positive whole number and outputs the following:
+
+The perimeter and area of a square whose side length is the entered number
+The radius, circumference, and area of a circle whose diameter is the entered number
+The perimeter and area of an equilateral triangle whose side length is equal to the entered number.
+The program will use the command line for its input and output (I/O). 
+'''
+import math
