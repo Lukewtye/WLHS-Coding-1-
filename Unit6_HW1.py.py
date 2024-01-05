5d944c28facaadd557f85d7380dfc9e583a2c032
Lukewtye <143744427+Lukewtye@users.noreply.github.com>
2024-01-05 11:22:42 -0800

    Add files via upload

---
+++ Unit6_HW1.py.py
@@ -0,0 +1,12 @@
+'''
+Name: Luke Tye
+Assignemnet: Unit6_HW1.py
+Date: 1/5/24
+'''
+list1 = int(input("give list1"))
+list2 = int(input("give list2"))
+def zip(list1, list2):
+#if len(list1) /= len(list2)
+    list_length = len(list1) * 2
+    for list_length in (list1, list2):
+        list
