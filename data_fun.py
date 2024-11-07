6c280adab6c351a6367598165a054b376d56d148
Luke Tye <143744427+Lukewtye@users.noreply.github.com>
2024-11-06 20:33:54 -0800

    Add files via upload
    This is the updated form of data.fun

--- data_fun.py
+++ data_fun.py
@@ -1,50 +1,81 @@
-'''
+"""
 Name: Luke Tye
 Date: 9/25/23
 Description: Ask user for favorite integer and character and print out a variety of information
 regarding those numbers.
 
-FOr upper and lowercase and numbers: user
-.isupper()
-.islower()
-.isnumeric()
-.isnumeric() (single number)
-'''
-
-#Part 1:
+"""
 integer = int(input("What's your favorite integer? "))
 if integer < 0:
-    print ("This integer is negative.")
+    print("\tThis integer is negative.")
 else:
-    print ("This integer is positive")
-if integer <10 and integer > 100:
-    print ("This integer is greater than 10 ")
-elif integer <100 and integer > 1000:
-    print ("This integer is greater than 100 ")
-elif integer <1000: 
-    print ("This number is greater than 1000")
-else: print ("This number is smaller than 10")
-#Odd or even:
-if (integer % 2) == 0:
-    print ("This number is even.")
-else: 
-    print ("This number is odd.")
-#Noble Gasses Matching
-match integer: 
+    print("\tThis integer is positive")
+
+if 10 < integer <= 100:
+    print("\tThis integer is greater than 10")
+elif 100 < integer <= 1000:
+    print("\tThis integer is greater than 100")
+elif integer > 1000:
+    print("\tThis number is greater than 1000")
+else:
+    print("\tThis number is smaller than 10")
+
+if integer % 2 == 0:
+    print("\tThis number is even.")
+else:
+    print("\tThis number is odd.")
+
+match integer:
     case 2:
-        print ("This is the Noble Gas Helium!")
+        print("\tThis is the Noble Gas Helium!")
     case 10:
-        print ("This is the Noble Gas Neon!")
+        print("\tThis is the Noble Gas Neon!")
     case 18:
-        print ("This is the Noble Gas Argon!")
+        print("\tThis is the Noble Gas Argon!")
     case 36:
-        print ("This is the Noble Gas Krypton!")
+        print("\tThis is the Noble Gas Krypton!")
     case 54:
-        print ("This is the Noble Gas Xenon!")
+        print("\tThis is the Noble Gas Xenon!")
     case 86:
-        print ("This is the Noble Gas Radon!")
+        print("\tThis is the Noble Gas Radon!")
     case 118:
-        print ("This is the Noble Gas Oganesson!")
+        print("\tThis is the Noble Gas Oganesson!")
     case _:
-        print ("This is not one of the Noble Gasses.")
-    
+        print("\tThis is not one of the Noble Gasses.")
+
+user_character = input("Give me your favorite character ")
+print(user_character)
+
+if user_character.isdigit():
+    print("\tYour character is a numeric digit.")
+elif user_character.islower():
+    print("\tYour character is a lowercase letter")
+    if user_character in "aeiou":
+        print("\tIt's a lowercase vowel")
+    else:
+        print("\tIt's a consonant.")
+elif user_character.isupper():
+    print("\tYour character is an uppercase letter")
+    if user_character in "AEIOU":
+        print("\tIt's an uppercase vowel")
+    else:
+        print("\tIt's a consonant.")
+else:
+    print("\tInvalid input.")
+
+ascii_value = ord(user_character)
+print("\tThe ASCII value of user_character is " + str(ascii_value))
+
+if user_character.islower() or user_character.isupper():
+    letter_position = ord(user_character) - ord("A") + 1
+    if letter_position == 1:
+        suffix = "st"
+    elif letter_position == 2:
+        suffix = "nd"
+    elif letter_position == 3:
+        suffix = "rd"
+    else:
+        suffix = "th"
+    print(
+        "\tAnd it is the " + str(letter_position) + suffix + " letter of the alphabet."
+    )
